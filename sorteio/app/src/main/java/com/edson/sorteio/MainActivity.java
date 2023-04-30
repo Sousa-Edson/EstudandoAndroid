a09ea234e18cc5db08ceba9b664f4771a41aa7f3
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-04-30 16:52:37 -0300

    adicionando sorteio

---
+++ sorteio/app/src/main/java/com/edson/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.edson.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+public void selecionarBotao(View v){
+        int x = new Random().nextInt(100);
+    TextView texto = findViewById(R.id.textoResultado);
+    texto.setText("O NUMERO SORTEADO FOI: "+x);
+}
+
+
+
+}
