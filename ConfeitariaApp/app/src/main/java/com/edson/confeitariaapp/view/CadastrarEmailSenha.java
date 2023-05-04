c0bb5e37bfcb76c9c38a5e88eefe040cc158640b
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-05-03 22:48:42 -0300

    Recriando o app confeitaria

---
+++ ConfeitariaApp/app/src/main/java/com/edson/confeitariaapp/view/CadastrarEmailSenha.java
@@ -0,0 +1,16 @@
+package com.edson.confeitariaapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.edson.confeitariaapp.R;
+
+public class CadastrarEmailSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_email_senha);
+    }
+}
