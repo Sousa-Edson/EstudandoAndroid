ec2e062f9cd5fae2f8bad62833e8a790b6299a56
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-05-18 14:52:43 -0300

    inicinando menu botton

---
+++ BottomNavigation/app/src/main/java/com/edson/bottomnavigation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.edson.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
